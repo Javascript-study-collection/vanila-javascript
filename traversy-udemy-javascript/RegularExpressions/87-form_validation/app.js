87a29ff13117d3f7a37e05b8ae71643c832cc4ba
haerangChoi <gofkdvjvl@naver.com>
2021-05-15 01:13:46 +0900

    feat: 주석 추가

--- traversy-udemy-javascript/RegularExpressions/87-form_validation/app.js
+++ traversy-udemy-javascript/RegularExpressions/87-form_validation/app.js
@@ -6,9 +6,10 @@ document.getElementById('phone').addEventListener('blur', validatePhone);
 
 function validateName() {
   const name = document.getElementById('name');
+  // 영문 대소문자 2~10글자
   const re = /^[a-zA-Z]{2,10}$/;
 
-  if(!re.test(name.value)){
+  if (!re.test(name.value)) {
     name.classList.add('is-invalid');
   } else {
     name.classList.remove('is-invalid');
@@ -19,7 +20,7 @@ function validateZip() {
   const zip = document.getElementById('zip');
   const re = /^[0-9]{5}(-[0-9]{4})?$/;
 
-  if(!re.test(zip.value)){
+  if (!re.test(zip.value)) {
     zip.classList.add('is-invalid');
   } else {
     zip.classList.remove('is-invalid');
@@ -30,7 +31,7 @@ function validateEmail() {
   const email = document.getElementById('email');
   const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 
-  if(!re.test(email.value)){
+  if (!re.test(email.value)) {
     email.classList.add('is-invalid');
   } else {
     email.classList.remove('is-invalid');
@@ -41,7 +42,7 @@ function validatePhone() {
   const phone = document.getElementById('phone');
   const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
 
-  if(!re.test(phone.value)){
+  if (!re.test(phone.value)) {
     phone.classList.add('is-invalid');
   } else {
     phone.classList.remove('is-invalid');
